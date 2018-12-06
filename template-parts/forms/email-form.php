b8131d3d79a0b79a6ad495dab8e061e379767618
Edmund Mai <edmundmai@gmail.com>
2018-12-05 19:16:49 -0500

    Finish outline of footer

---
+++ template-parts/forms/email-form.php
@@ -0,0 +1,7 @@
+<div class="doordash-sign-up-container">
+  <h5 class="doordash-sign-up-text">Sign up for updates</h5>
+  <form class="doordash-email-form">
+    <input type="text" name="email" placeholder="Your email address" class="doordash-subscription-input" />
+    <input type="submit" value="Subscribe" class="doordash-subscription-submit" />
+  </form>
+</div>
--- template-parts/headers/header-normal.php
+++ template-parts/headers/header-normal.php
@@ -90,13 +90,7 @@
 
           </div>
 
-          <div>
-            <h5 class="doordash-sign-up-text">Sign up for updates</h5>
-            <form class="doordash-email-form">
-              <input type="text" name="email" placeholder="Your email address" class="doordash-subscription-input" />
-              <input type="submit" value="Subscribe" class="doordash-subscription-submit" />
-            </form>
-          </div>
+          <?php get_template_part( 'template-parts/forms/email-form' ); ?>
 
         </div><!-- /.header-upper -->
 
