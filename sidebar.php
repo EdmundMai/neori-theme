1c1cb46ca1c1615fca4dc526071efe129bef374a
Edmund Mai <edmundmai@gmail.com>
2018-12-06 18:38:50 -0500

    Adds related posts to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,54 @@
-<?php if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+<div>
+  <h4>Related Posts</h4>
 
-    return;
-  }
-?>
+  <div class="doordash-related-posts-container">
 
-<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php
+
+      $orig_post = $post;
+      global $post;
+      $categories = get_the_category($post->ID);
+
+      if ($categories) {
+
+        $category_ids = array();
+
+        foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+
+        $args=array(
+          'category__in' => $category_ids,
+          'post__not_in' => array($post->ID),
+          'posts_per_page'=> 3,
+          'ignore_sticky_posts'=> 1
+        );
+
+        $related_posts_query = new WP_Query( $args );
+
+        while( $related_posts_query->have_posts() ) {
+
+          $related_posts_query->the_post();
+
+    ?>
+
+            <?php if(has_post_thumbnail()) : ?>
+
+              <div class="doordash-related-post-row">
+
+                <div>
+                  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'doordash-related-post-img')); ?></a>
+                </div>
+                <h4 class="doordash-related-post-title"><a rel="external" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+
+              </div><!-- /.custom-card -->
+
+            <?php endif; ?>
+
+    <?php }
+                }
+      $post = $orig_post;
+      wp_reset_postdata();
+    ?>
+
+  </div><!-- /.card-deck -->
+
+</div><!-- /.related-posts -->
