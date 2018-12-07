90a34c8094120d3ed06f7ef28f05ff91dcd29401
Edmund Mai <edmundmai@gmail.com>
2018-12-07 12:39:35 -0500

    Updates URL for email form

--- template-parts/forms/email-form.php
+++ template-parts/forms/email-form.php
@@ -1,6 +1,6 @@
 <div class="doordash-sign-up-container">
   <h5 class="doordash-sign-up-text">Sign up for updates</h5>
-  <form class="doordash-email-form">
+  <form class="doordash-email-form" name="iterable_optin" action="//links.iterable.com/lists/addSubscriberForm?emailListId=198033" target="_blank" method="POST">
     <input type="text" name="email" placeholder="Your email address" class="doordash-subscription-input" />
     <input type="submit" value="Subscribe" class="doordash-subscription-submit" />
   </form>
