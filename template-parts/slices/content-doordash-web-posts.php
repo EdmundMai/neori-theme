6b04a78cf2e211c01f7649e2e380c77d74712940
Edmund Mai <edmundmai@gmail.com>
2018-12-20 17:24:37 -0500

    Use shorter month names on home page for more real estate

--- template-parts/slices/content-doordash-web-posts.php
+++ template-parts/slices/content-doordash-web-posts.php
@@ -26,9 +26,9 @@
 
         <?php echo get_avatar( get_the_author_meta('user_email'), '35', '' ); ?>
         <span class="author"><?php the_author_posts_link(); ?></span>
-        <span class="date"><?php the_time( get_option('date_format') ); ?></span>
-        <span>&nbsp;·&nbsp; </span>
-        <span class="doordash-read-time"><?php echo (ceil(strlen(get_the_content()) / 2500)); ?> min read</span>
+        <span class="date"><?php the_time( 'M j, Y' ); ?>
+        &nbsp;·&nbsp; 
+        <?php echo (ceil(strlen(get_the_content()) / 2500)); ?> min read</span>
 
 
       </p><!-- /.card-meta -->
